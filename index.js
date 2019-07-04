a992c1a9bc5feebbaf7035b22631b361ed0d37c1
Andy Choi <wooseokchoi96@gmail.com>
2019-07-04 15:44:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ function takeANumber (katzDeliLine,customer) {
   return `Welcome, ${customer}. You are number ${katzDeliLine.length} in line.`;
 }
 
+function takeANumberNew (katzDeliLine) {
+  var counter;
+  counter+=1;
+  katzDeliLine.push(counter);
+  return 'Welcome you have ticket number ${counter}.'
+}
+
 function nowServing (katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!";
